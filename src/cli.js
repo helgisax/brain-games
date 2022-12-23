8eb0c6f57e70a28aef9fb0a284fbac70620b0ebf
helgisax <helgiingvarsson77@gmail.com>
2022-12-23 07:20:59 +0300

    Corrected after checking with linter

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
+
 const askAndGreetByName = () => {
-    let userName = readlineSync.question('May I have your name? ');
-    console.log('Hello, ' + userName + '!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
 };
 export default askAndGreetByName;
