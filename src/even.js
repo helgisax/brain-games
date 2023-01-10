710fe4bcb594c71c41384e6bc52ed727ed2bc5a3
helgisax <helgiingvarsson77@gmail.com>
2023-01-10 20:11:12 +0300

    update

--- src/even.js
+++ src/even.js
@@ -7,16 +7,25 @@ const letsPlayEven = () => {
   console.log(`Hello, ${userName}!`);
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-  const theRandomNumber = Math.ceil(Math.random() * 33);
-  console.log(`Question: ${theRandomNumber}`);
-
-  const userAnswer = readlineSync.question('Your answer: ');
-  if (theRandomNumber % 2 === 0 || userAnswer === 'yes') {
-    console.log('Correct!');
-  } else if (!theRandomNumber % 2 === 0 || userAnswer === 'no') {
-    console.log('Correct!');
-  } else {
-    console.log(`'yes' is wrong answer ;(. Correct answer was 'no'. Let's try again, ${userName}'`);
-  }
+  const askUser = () => {
+    let i = 0;
+    while (i < 3) {
+      const theRandomNumber = Math.ceil(Math.random() * 33);
+      console.log(`Question: ${theRandomNumber}`);
+      const userAnswer = readlineSync.question('Your answer: ');
+      if (theRandomNumber % 2 === 0 || userAnswer === 'yes') {
+        console.log('Correct!');
+        i += 1;
+      } else if (!theRandomNumber % 2 === 0 || userAnswer === 'no') {
+        console.log('Correct!');
+        i += 1;
+      } else {
+        console.log(`'yes' is wrong answer ;(. Correct answer was 'no'. Let's try again, ${userName}'`);
+        break;
+      }
+    }
+    console.log(`Congratulations, ${userName}!`);
+  };
+  askUser();
 };
 export default letsPlayEven;
