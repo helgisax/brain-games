fa2f715ab6818ed8cdb09b64a919b19c53da123c
helgisax <helgiingvarsson77@gmail.com>
2023-01-19 02:52:58 +0300

    updated

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
 import readlineSync from 'readline-sync';
-// надо ли сюда импортировать функции из папки games? пока не ясно как это всё связать между собой.
 
-const sample = (/* сюда аргумент с consolelog и правилами, но пока не понимаю, откуад его брать. rules*/) => {
+const sample = (displayRules, giveQuestAndAns) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
-  console.log(rules);
+  displayRules();
 
   const checkTheGame = () => {
     for (let i = 0; i < 3; i += 1) {
+      const questAndAns = giveQuestAndAns();
+      console.log(`Question: ${questAndAns[0]}`);
       const userAnswer = readlineSync.question('Your answer: ');
-
-//вот сюда по идее надо втыкать функцию-вопрос, которая должна как-то вернуть видимо праивльный ответ, но я пока не оч понимаю механизм взаимодействия функций между собой и как сделать так, чтобы у меня в этой функции тоже появился правильный ответ, и чтобы они синхронизировались ещё
-
-      if (currentAnswer === userAnswer) {
+      if (questAndAns[1] === userAnswer) {
         console.log('Correct');
-      } else if (currentAnswer !== userAnswer) {
-        console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${currentAnswer}. Let's try again, ${userName}`);
+      } else if (questAndAns[1] !== userAnswer) {
+        console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${questAndAns[1]}. Let's try again, ${userName}`);
         break;
       }
       console.log(`Congratulations, ${userName}!`);
