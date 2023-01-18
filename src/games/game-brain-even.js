01c7033399d65e818ebae3fc57dfe7990c0c7cd6
helgisax <helgiingvarsson77@gmail.com>
2023-01-19 02:53:52 +0300

    changed by analogy with the new task

--- src/games/game-brain-even.js
+++ src/games/game-brain-even.js
@@ -1,8 +1,18 @@
-const giveQuestAndAns = () => {
-  const theRandomNumber = Math.ceil(Math.random() * 33);
-  const currentAnswer = theRandomNumber % 2 === 0 ? 'yes' : 'no';
+import sample from '../index.js';
 
-  console.log(`Question: ${theRandomNumber}`);
-  return currentAnswer;
+const letsPlayEven = () => {
+  const displayRules = () => {
+    console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  };
+
+  const giveQuestAndAns = () => {
+    const theRandomNumber = Math.ceil(Math.random() * 33);
+    const currentAnswer = theRandomNumber % 2 === 0 ? 'yes' : 'no';
+    return [
+      theRandomNumber,
+      currentAnswer,
+    ];
+  };
+  sample(displayRules, giveQuestAndAns);
 };
-export default giveQuestAndAns;
+export default letsPlayEven;
