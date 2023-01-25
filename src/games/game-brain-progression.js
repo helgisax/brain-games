9eb3bad4db0c72bba4295e451e9e028215827fd8
helgisax <helgiingvarsson77@gmail.com>
2023-01-25 23:07:31 +0300

    created brain-progression

---
+++ src/games/game-brain-progression.js
@@ -0,0 +1,37 @@
+import sample from '../index.js';
+
+const letsPlayProgression = () => {
+  const displayRules = () => {
+    console.log('What number is missing in the progression?');
+  };
+
+  const calculator = (begin, step, length) => {
+    const result = [begin];
+    for (let i = 1; i < length; i += 1) {
+      result.push(result[i - 1] + step);
+    }
+    return result;
+  };
+
+  const getRandomNumber = (num) => Math.ceil(Math.random() * num);
+
+  const giveQuestAndAns = () => {
+    const actualBegin = getRandomNumber(33);
+    const actualStep = getRandomNumber(5) + 1;
+    const actualLength = getRandomNumber(10) + 5;
+    const hiddenNumber = getRandomNumber(actualLength);
+
+    const actualProgression = calculator(actualBegin, actualStep, actualLength);
+    const hiddenAnswer = actualProgression[hiddenNumber];
+    actualProgression[hiddenNumber] = '..';
+
+    const question = actualProgression.join(' ');
+    const answer = hiddenAnswer.toString();
+    return [
+      question,
+      answer,
+    ];
+  };
+  sample(displayRules, giveQuestAndAns);
+};
+export default letsPlayProgression;
