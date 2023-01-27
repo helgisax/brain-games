c44a142cf15e6411c4d872be47ccb50c8facf1b2
helgisax <helgiingvarsson77@gmail.com>
2023-01-27 04:06:30 +0300

    fixed string()

--- src/games/game-brain-progression.js
+++ src/games/game-brain-progression.js
@@ -26,7 +26,7 @@ const letsPlayProgression = () => {
     actualProgression[hiddenNumber] = '..';
 
     const question = actualProgression.join(' ');
-    const answer = hiddenAnswer.toString();
+    const answer = hiddenAnswer ? hiddenAnswer.toString() : '';
     return [
       question,
       answer,
