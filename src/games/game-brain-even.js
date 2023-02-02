3fafaa392a7ace81e3e3f2fba97e9c48d144a869
helgisax <helgiingvarsson77@gmail.com>
2023-02-02 16:13:16 +0300

    renamed

--- src/games/game-brain-even.js
+++ src/games/game-brain-even.js
@@ -1,11 +1,8 @@
-import sample from '../index.js';
+import getGameEngine from '../index.js';
 import { getRandomNumber } from '../instruments/instruments.js';
 
-const letsPlayEven = () => {
-  const displayRules = () => {
-    console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  };
-
+const playEvenGame = () => {
+  const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
   const giveQuestAndAns = () => {
     const theRandomNumber = getRandomNumber(33);
     const currentAnswer = theRandomNumber % 2 === 0 ? 'yes' : 'no';
@@ -14,6 +11,6 @@ const letsPlayEven = () => {
       currentAnswer,
     ];
   };
-  sample(displayRules, giveQuestAndAns);
+  getGameEngine(rules, giveQuestAndAns);
 };
-export default letsPlayEven;
+export default playEvenGame;
