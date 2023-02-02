104373afc55aef7fa41ca23a60f9453f83c746ea
helgisax <helgiingvarsson77@gmail.com>
2023-02-02 21:32:22 +0300

    fixed after review

--- src/instruments/instruments.js
+++
@@ -1,6 +0,0 @@
-export const getRandomOperator = (arr) => {
-  const index = Math.ceil(Math.random() * arr.length - 1);
-  return arr[index];
-};
-
-export const getRandomNumber = (num) => Math.ceil(Math.random() * num);
