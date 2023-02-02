e168f795bac6d2b11e6b33480ebab1edfc6f3856
helgisax <helgiingvarsson77@gmail.com>
2023-02-02 21:28:18 +0300

    fixed after review

---
+++ src/games/brain-calc.js
@@ -0,0 +1,36 @@
+import getGameEngine from '../index.js';
+import getRandomNumber from '../getRandomNumber.js';
+
+const rules = 'What is the result of the expression?';
+
+const getCalculations = (num1, num2, operator) => {
+  switch (operator) {
+    case '+':
+      return num1 + num2;
+    case '-':
+      return num1 - num2;
+    case '*':
+      return num1 * num2;
+    default:
+      throw new Error('Unknown operation!');
+  }
+};
+
+const operations = ['+', '-', '*'];
+const buildRoundData = () => {
+  const randomIndex = getRandomNumber(0, 2);
+  const firstNum = getRandomNumber(1, 33);
+  const secondNum = getRandomNumber(1, 33);
+  const randomOperator = operations[randomIndex];
+  const question = `${firstNum} ${randomOperator} ${secondNum}`;
+  const answer = getCalculations(firstNum, secondNum, randomOperator).toString();
+  return [
+    question,
+    answer,
+  ];
+};
+
+const playCalcGame = () => {
+  getGameEngine(rules, buildRoundData);
+};
+export default playCalcGame;
