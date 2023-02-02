d2f0dba7d7274c698282890cc6b52c4da1ac6450
helgisax <helgiingvarsson77@gmail.com>
2023-02-02 21:29:21 +0300

    fixed after review

---
+++ src/games/brain-progression.js
@@ -0,0 +1,35 @@
+import getGameEngine from '../index.js';
+import getRandomNumber from '../getRandomNumber.js';
+
+const rules = 'What number is missing in the progression?';
+
+const getCalculations = (begin, step, length) => {
+  const result = [begin];
+  for (let i = 1; i < length; i += 1) {
+    result.push(result[i - 1] + step);
+  }
+  return result;
+};
+
+const buildRoundData = () => {
+  const begin = getRandomNumber(1, 33);
+  const step = getRandomNumber(1, 5);
+  const length = getRandomNumber(5, 10);
+  const hiddenNumber = getRandomNumber(1, length);
+
+  const progression = getCalculations(begin, step, length);
+  const hiddenAnswer = progression[hiddenNumber];
+  progression[hiddenNumber] = '..';
+
+  const question = progression.join(' ');
+  const answer = String(hiddenAnswer);
+  return [
+    question,
+    answer,
+  ];
+};
+
+const playProgressionGame = () => {
+  getGameEngine(rules, buildRoundData);
+};
+export default playProgressionGame;
