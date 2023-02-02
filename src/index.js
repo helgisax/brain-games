48fe29b0b79e0bc9aa8e4c647ce2d0fe91a0de2b
helgisax <helgiingvarsson77@gmail.com>
2023-02-02 16:31:22 +0300

    desdestructuring

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-const getGameEngine = (rules, giveQuestAndAns) => {
+const getGameEngine = (rules, buildRoundData) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
@@ -9,13 +9,15 @@ const getGameEngine = (rules, giveQuestAndAns) => {
   const numberOfRounds = 3;
 
   for (let i = 0; i < numberOfRounds; i += 1) {
-    const questAndAns = giveQuestAndAns();
-    console.log(`Question: ${questAndAns[0]}`);
+    const questAndAns = buildRoundData();
+    const [question, answer] = questAndAns;
+
+    console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
     if (questAndAns[1] === userAnswer) {
       console.log('Correct');
-    } else if (questAndAns[1] !== userAnswer) {
-      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${questAndAns[1]}.\nLet's try again, ${userName}!`);
+    } else if (answer !== userAnswer) {
+      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${answer}.\nLet's try again, ${userName}!`);
       return;
     }
   }
