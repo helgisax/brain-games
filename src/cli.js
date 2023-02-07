48eb6541f1ff694f3bbd449bcb7018b12062a43a
helgisax <helgiingvarsson77@gmail.com>
2023-02-08 01:56:02 +0300

    fixed funcs name

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
 import readlineSync from 'readline-sync';
 
 console.log('Welcome to the Brain Games!');
-const askAndGreetByName = () => {
+const playBrainGames = () => {
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 };
-export default askAndGreetByName;
+export default playBrainGames;
