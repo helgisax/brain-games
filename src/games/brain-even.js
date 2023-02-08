dfc976c07c7959fbfa6d2fc98c5b4717f060c3a8
helgisax <helgiingvarsson77@gmail.com>
2023-02-08 03:02:55 +0300

    last fixed

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,15 +1,12 @@
 import getGameEngine from '../index.js';
 import getRandomNumber from '../getRandomNumber.js';
 
-const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
+const task = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+const isEven = (num) => num % 2 === 0;
 
 const buildRoundData = () => {
   const randomNumber = getRandomNumber(1, 33);
-  const isEven = (num) => {
-    if (num % 2 === 0) {
-      return true;
-    } return false;
-  };
   const currentAnswer = isEven(randomNumber) ? 'yes' : 'no';
   return [
     randomNumber,
@@ -18,6 +15,6 @@ const buildRoundData = () => {
 };
 
 const playEvenGame = () => {
-  getGameEngine(rules, buildRoundData);
+  getGameEngine(task, buildRoundData);
 };
 export default playEvenGame;
