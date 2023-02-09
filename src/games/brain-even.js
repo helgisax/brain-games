71816e74852a3e7c9f4155e03cc1a4b86a250778
helgisax <helgiingvarsson77@gmail.com>
2023-02-09 19:13:13 +0300

    fixed arguement in randomfunc

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -6,7 +6,7 @@ const task = 'Answer "yes" if the number is even, otherwise answer "no".';
 const isEven = (num) => num % 2 === 0;
 
 const buildRoundData = () => {
-  const randomNumber = getRandomNumber(1, 33);
+  const randomNumber = getRandomNumber(33);
   const currentAnswer = isEven(randomNumber) ? 'yes' : 'no';
   return [
     randomNumber,
