83c2b699a36ff1de00735257196fb8678ae952bb
helgisax <helgiingvarsson77@gmail.com>
2023-02-09 19:14:59 +0300

    fixed default value

--- src/getRandomNumber.js
+++ src/getRandomNumber.js
@@ -1,4 +1,4 @@
-const getRandomNumber = (minNum, maxNum) => {
+const getRandomNumber = (maxNum, minNum = 1.33) => {
   const num = Math.random() * (maxNum - minNum + 1) + minNum;
   return Math.floor(num);
 };
