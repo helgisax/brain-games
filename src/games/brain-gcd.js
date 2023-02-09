a4b905b8cb17ad497b6a935a769cd73c7d2357f1
helgisax <helgiingvarsson77@gmail.com>
2023-02-09 19:13:51 +0300

    fixed arguement in randomfunc and method Math.min()

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -3,10 +3,8 @@ import getRandomNumber from '../getRandomNumber.js';
 
 const task = 'Find the greatest common divisor of given numbers';
 
-const minNum = (num1, num2) => (num1 > num2 ? num2 : num1);
-
-const getCalculations = (num1, num2) => {
-  for (let i = minNum(num1, num2); i > 0; i -= 1) {
+const getGcd = (num1, num2) => {
+  for (let i = Math.min(num1, num2); i > 0; i -= 1) {
     if (num1 % i === 0 && num2 % i === 0) {
       return i;
     }
@@ -15,10 +13,10 @@ const getCalculations = (num1, num2) => {
 };
 
 const buildRoundData = () => {
-  const firstNum = getRandomNumber(1, 33);
+  const firstNum = getRandomNumber(33);
   const secondNum = getRandomNumber(33, 66);
   const question = `${firstNum} ${secondNum}`;
-  const answer = getCalculations(firstNum, secondNum).toString();
+  const answer = getGcd(firstNum, secondNum).toString();
   return [
     question,
     answer,
