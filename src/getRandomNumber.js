c33b815bb668d0cc2be14a5c16eee29e202203c7
helgisax <helgiingvarsson77@gmail.com>
2023-02-11 19:30:59 +0300

    fixed stupid thing with getNumber

--- src/getRandomNumber.js
+++ src/getRandomNumber.js
@@ -1,4 +1,4 @@
-const getRandomNumber = (maxNum, minNum = 1.33) => {
+const getRandomNumber = (minNum = 1, maxNum = 33) => {
   const num = Math.random() * (maxNum - minNum + 1) + minNum;
   return Math.floor(num);
 };
